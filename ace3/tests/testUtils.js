6de10e92c2b346d4bc8d5d99c8fb14301a7ce689
Mizar01 <zanarotti.michele@gmail.com>
2013-12-25 12:01:36 -0800

    Added the class TestUtils

---
+++ ace3/tests/testUtils.js
@@ -0,0 +1,10 @@
+ACE3.TestUtils = []
+
+ACE3.TestUtils.makeTestCube = function(position, color){
+    var color = color || 0xffffff
+    var obj = ACE3.Builder.cube(1, color)
+    obj.position = position
+    _ace3.scene.add(obj)
+
+    return obj
+}
